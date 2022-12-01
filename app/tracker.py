459a2e4d73827486586720523a90fbc462450e70
nanorex07 <jainnamah@gmail.com>
2022-12-01 21:15:11 +0530

    navigation done

--- app/tracker.py
+++ app/tracker.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, flash, render_template, request
+from flask import Blueprint, flash, render_template, request, abort
 from flask_login import current_user, login_required
-from .models import db, Item
+from .models import db, Item, User
 
 tracker = Blueprint("tracker", __name__)
+
+
+@tracker.route("/<id>", methods=["GET"])
+def track_with_id(id):
+    user = User.query.filter_by(id=id).first()
+    if user:
+        return render_template("user.html", user=user)
+
+    return abort(404)
 
 
 @login_required
@@ -17,5 +26,7 @@ def track():
             item = Item(name=name, calories=calories, user=current_user)
             db.session.add(item)
             db.session.commit()
-
+    pass_user = request.args.get("user")
+    if pass_user:
+        return render_template("tracker.html", user=pass_user)
     return render_template("tracker.html", user=current_user)
