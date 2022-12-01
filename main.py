035b918de5a8e5340ce0be4fa42a7ac350fbda26
nanorex07 <jainnamah@gmail.com>
2022-12-01 21:16:55 +0530

    navigation done

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from app import app as application
-from waitress import serve
 
 if __name__ == "__main__":
-    serve(application, host="0.0.0.0", port=8000)
+    application.run(debug=False, host="0.0.0.0")
